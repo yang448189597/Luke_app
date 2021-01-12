6e25adc0f38074277b717fd822159d19c5d926a7
DongLy <448189597@qq.com>
2021-01-12 11:32:41 +0800

    仿weibo嵌套滑动控件

--- app/src/main/java/com/example/luke_app/main/shanghai/ShangHaiFragment.java
+++ app/src/main/java/com/example/luke_app/main/shanghai/ShangHaiFragment.java
@@ -1,10 +1,15 @@
 package com.example.luke_app.main.shanghai;
 
+import android.view.View;
+import android.widget.TextView;
+
 import com.example.luke_app.R;
 import com.example.luke_app.base.BaseFragment;
 import com.example.luke_app.base.ViewInject;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
-import androidx.fragment.app.Fragment;
+import butterknife.BindView;
 
 /*
  * @Author: Luke
@@ -15,8 +20,24 @@ import androidx.fragment.app.Fragment;
 @ViewInject(mainLayoutid = R.layout.fragment_shanghai)
 public class ShangHaiFragment extends BaseFragment {
 
+    @BindView(R.id.tv_shanghai_welcome)
+    TextView tvShanghaiWelcome;
+    @BindView(R.id.shanghai_collapsingToolbarLayout)
+    CollapsingToolbarLayout shanghaiCollapsingToolbarLayout;
+    @BindView(R.id.shanghai_app_bar_layout)
+    AppBarLayout shanghaiAppBarLayout;
+
     @Override
     public void afterBindView() {
-
+        shanghaiAppBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (-verticalOffset < appBarLayout.getMeasuredHeight() / 2) {
+                    tvShanghaiWelcome.setVisibility(View.INVISIBLE);
+                } else {
+                    tvShanghaiWelcome.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
